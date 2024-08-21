b3fea57db9b84c73d0f8e5cb51827d572f7b614a
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2024-08-21 18:57:58 +0200

    build: Set some clippy lints to warn/deny level
    Lints set to warn level:
        * `missing_docs` (library only)
        * `clippy::missing_docs_in_private_items` (library only)
        * `clippy::unwrap_used`
        * `clippy::expect_used`
        * `clippy::too_many_lines`
        * `clippy::derive_partial_eq_without_eq` (library only)
        * `clippy::use_self` (library only)
        * `clippy::use_debug`
        * `clippy::dbg_macro`
    
    Lints set to warn level in debug mode:
        * `clippy::todo`
        * `clippy::unimplemented`
    
    Lints set to deny level in release mode:
        * `clippy::todo`
        * `clippy::unimplemented`
    
    Lints noted to be set to warn level once stable:
        * `clippy::allow_attributes_without_reason` (stable in rust 1.81.0)

--- src/bin/dev/main.rs
+++ src/bin/dev/main.rs
@@ -1,3 +1,16 @@
+#![warn(clippy::unwrap_used)]
+#![warn(clippy::expect_used)]
+#![warn(clippy::too_many_lines)]
+// `allow_attributes_without_reason` requires the feature `lint_reason`.
+// Activating this lint should be reconsidered once `lint_reason` is stabilized.
+// #![warn(clippy::allow_attributes_without_reason)]
+#![warn(clippy::use_debug)]
+#![warn(clippy::dbg_macro)]
+#![warn(clippy::todo)]
+#![warn(clippy::unimplemented)]
+#![cfg_attr(not(debug_assertions), deny(clippy::todo))]
+#![cfg_attr(not(debug_assertions), deny(clippy::unimplemented))]
+
 mod clap;
 
 fn main() -> Result<(), anyhow::Error> {
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+//! Conveniently automate tasks using shell scripts.
+
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
+#![warn(clippy::unwrap_used)]
+#![warn(clippy::expect_used)]
+#![warn(clippy::derive_partial_eq_without_eq)]
+#![warn(clippy::use_self)]
+#![warn(clippy::too_many_lines)]
+// `allow_attributes_without_reason` requires the feature `lint_reason`.
+// Activating this lint should be reconsidered once `lint_reason` is stabilized.
+// #![warn(clippy::allow_attributes_without_reason)]
+#![warn(clippy::use_debug)]
+#![warn(clippy::dbg_macro)]
+#![warn(clippy::todo)]
+#![warn(clippy::unimplemented)]
+#![cfg_attr(not(debug_assertions), deny(clippy::todo))]
+#![cfg_attr(not(debug_assertions), deny(clippy::unimplemented))]
